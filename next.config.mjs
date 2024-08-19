99a84bd71a5335ec057cb62325c0a077362f498b
Abhinav Singh <official.abhinavsingh15@gmail.com>
2024-08-19 19:01:54 +0530

    chore: Update next.config.mjs and package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "images.pexels.com",
+    },
+    {
+      protocol: "https",
+      hostname: "*.ap-south-1.amazonaws.com",
+    },
+    {
+      protocol: "https",
+      hostname: "avatars.githubusercontent.com",
+    },
+    {
+      protocol: "https",
+      hostname: "www.frontend.fyi"
+    }
+  ],
+},};
 
 export default nextConfig;
